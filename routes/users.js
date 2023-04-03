c8655cec973692fe2481dbc40f19333541d3ec48
Pelayodc <uo281956@uniovi.es>
2023-04-03 13:23:40 +0200

    SDI-2223-703-9.3-Mensajes y alertas.

--- routes/users.js
+++ routes/users.js
@@ -13,9 +13,9 @@ module.exports = function (app, usersRepository) {
     let securePassword = app.get("crypto").createHmac('sha256',app.get('clave')).update(req.body.password).digest('hex');
     let user = {email:req.body.email, password: securePassword}
     usersRepository.insertUser(user).then(userId => {
-      res.redirect("users/login");
+      res.redirect("users/login"+"?message=Nuevo usuario registrado."+"&messageType=alert-info");
     }).catch(error => {
-      res.send("Error al insertar usuario");
+      res.redirect("users/signup"+"?message=Se ha producido un error al registrar al usuario."+"&messageType=alert-danger");
     });
   });
   app.post('/users/login',function (req,res) {
@@ -28,14 +28,18 @@ module.exports = function (app, usersRepository) {
     usersRepository.findUser(filter, options).then(user => {
       if(user ==null) {
         req.session.user = null;
-        res.send("Usuario no identificado");
+        res.redirect("/users/login" +
+            "?message=Email o password incorrecto"+
+            "&messageType=alert-danger ");
       } else {
         req.session.user = user.email;
         res.redirect("/publications");
       }
     }).catch(error => {
       req.session.user = null;
-      res.render("Se ha producido un error al buscar el usuario: "+ error)
+      res.redirect("/users/login" +
+          "?message=Se ha producido un error al buscar el usuario"+
+          "&messageType=alert-danger ");
     });
   });
   app.get('/users/logout', function (req, res) {
