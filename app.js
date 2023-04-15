ad923bca8e5075b78333e0a6a457dc108e6873a4
Pelayodc <uo281956@uniovi.es>
2023-04-15 13:02:55 +0200

    SDI-2223-703-10.3-SW Cliente JQuery + AJAX.

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+  next();
+});
+
 let jwt = require('jsonwebtoken'); app.set('jwt', jwt);
 let expressSession = require('express-session');
 
